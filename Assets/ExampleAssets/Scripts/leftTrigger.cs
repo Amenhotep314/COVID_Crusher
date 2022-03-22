6587f4023f4d2f9ba544e9bb68f892e2175a6e8e
Amenhotep314 <nathan.lenzini@gmail.com>
2022-03-22 14:57:02 -0400

    [Class] Adds colliders

---
+++ Assets/ExampleAssets/Scripts/leftTrigger.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class leftTrigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //https://www.youtube.com/watch?v=EINgIoTG8D4
+        //https://docs.unity3d.com/Manual/class-LineRenderer.html
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
