c97ebf2dbb08191d86e3faf21382caae839395aa
Amenhotep314 <nathan.lenzini@gmail.com>
2022-03-30 15:12:26 -0400

    [Class] Adds line rendering

---
+++ Assets/ExampleAssets/Scripts/LeftController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+
+public class LeftController : MonoBehaviour
+{
+    public LineRenderer currentLeftLine;
+
+        void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        RaycastHit hit;
+        Ray shooterDirection = new Ray(transform.position, transform.forward);
+        bool target = Physics.Raycast(transform.position, transform.forward, out hit);
+
+        currentLeftLine.SetPosition(0, transform.position);
+        if(target) currentLeftLine.SetPosition(1, hit.point);
+        else currentLeftLine.SetPosition(1, transform.localPosition + new Vector3(0, 0, 90));
+
+        bool triggerValue;
+        if(XRNode.LeftHand.TryGetFeatureValue(UnityEngine.XR.CommonUsages.triggerButton, out triggerValue) && triggerValue)
+        {
+            
+        }
+    }
+}
