48cc494c1a0ec3f01524799b4718142e8fa4eec4
Amenhotep314 <nathan.lenzini@gmail.com>
2022-03-30 19:07:10 -0400

    [Home] 2022-03-30

--- Assets/ExampleAssets/Scripts/LeftController.cs
+++ Assets/ExampleAssets/Scripts/LeftController.cs
@@ -18,16 +18,15 @@ public class LeftController : MonoBehaviour
     {
         RaycastHit hit;
         Ray shooterDirection = new Ray(transform.position, transform.forward);
-        bool target = Physics.Raycast(transform.position, transform.forward, out hit);
+        bool target = Physics.Raycast(transform.position, transform.forward, out hit, 90, 1, QueryTriggerInteraction.Collide);
+        Debug.DrawRay(transform.position, transform.forward);
+        print(target);
 
         currentLeftLine.SetPosition(0, transform.position);
         if(target) currentLeftLine.SetPosition(1, hit.point);
         else currentLeftLine.SetPosition(1, transform.localPosition + new Vector3(0, 0, 90));
 
         bool triggerValue;
-        if(XRNode.LeftHand.TryGetFeatureValue(UnityEngine.XR.CommonUsages.triggerButton, out triggerValue) && triggerValue)
-        {
-            
-        }
+        //if(XRNode.LeftHand.TryGetFeatureValue(UnityEngine.XR.CommonUsages.triggerButton, out triggerValue) && triggerValue){}
     }
 }
